e91f46749d4346451a746d3a1c4a945702f4defb
Jonathan Wilson <jdw7@rice.edu>
2013-11-15 20:20:57 -0600

    Added twinkle song

---
+++ src/js/songs.js
@@ -0,0 +1,26 @@
+// CALCULATIONS
+//
+// bpm = 60 * beats/(tb - ta)
+
+
+//Twinkle Twinkle Little Star
+//
+// ta = 3.37 sec, tb = 9.70 sec, 10 beats
+
+// Frequencies
+
+var Notes = {
+
+};
+
+
+var Songs = [
+	{
+				startTime : 0,
+                bpm :  94.79
+                title : "Twinkle Twinkle Little Star",
+                notes : [[C,1],[C,1],[G,1],[G,1],[A,1],[A,1],[G,2],[F,1],[F,1],[E,1],[E,1],[D,1],[D,1],[C,2],
+						 [G,1],[G,1],[F,1],[F,1],[E,1],[E,1],[D,2],[G,1],[G,1],[F,1],[F,1],[E,1],[E,1],[D,2],
+						 [C,1],[C,1],[G,1],[G,1],[A,1],[A,1],[G,2],[F,1],[F,1],[E,1],[E,1],[D,1],[D,1],[C,2]]
+
+];
